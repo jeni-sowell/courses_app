ecf95f6f1480ec80d71cca93d693e5b8c28e2382
JeniSowell <jeni@sowellapp.com>
2023-10-03 14:17:35 +0300

    public_activity for Lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,10 @@
 class Lesson < ApplicationRecord
+  include PublicActivity::Model
   belongs_to :course
+
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+
+  def to_s
+    title
+  end
 end
